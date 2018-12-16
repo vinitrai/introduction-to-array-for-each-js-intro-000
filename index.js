20d6acfb08fdcd5615978b8483e5fbccf905c6c1
Vinit Rai <learner.vinit@gmail.com>
2018-12-16 13:46:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,3 @@ function doToElementsInArray(array,callbackFn){
 function changeCompletely(element,index,array){
   array[index] = `Math.floor(Math.random()*100).toString()+${array[index]}s!!!`;
 }
-
